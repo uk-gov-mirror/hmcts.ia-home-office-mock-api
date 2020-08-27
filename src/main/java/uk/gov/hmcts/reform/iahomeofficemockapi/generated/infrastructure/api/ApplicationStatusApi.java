fbaa0fa7196e4700f1a3d97e32e9acb411bedb7b
Jagadeesh Kumar <jagadeesh.kumar@transformuk.com>
2020-08-27 17:16:46 +0100

    RIA-3564: Added additional responses for different scenarios and support for /token endpoint and to receive authorization header

--- src/main/java/uk/gov/hmcts/reform/iahomeofficemockapi/generated/infrastructure/api/ApplicationStatusApi.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficemockapi/generated/infrastructure/api/ApplicationStatusApi.java
@@ -5,8 +5,15 @@
  */
 package uk.gov.hmcts.reform.iahomeofficemockapi.generated.infrastructure.api;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.ResourceUtils;
 import uk.gov.hmcts.reform.iahomeofficemockapi.generated.domain.entities.SearchErrorResponse;
 import uk.gov.hmcts.reform.iahomeofficemockapi.generated.domain.entities.SearchParameters;
 import uk.gov.hmcts.reform.iahomeofficemockapi.generated.domain.entities.SearchResponse;
@@ -15,21 +22,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import javax.validation.constraints.*;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-07-01T14:15:45.837+01:00[Europe/London]")
 
@@ -50,169 +49,121 @@ public interface ApplicationStatusApi {
      *         or Invalid status value (status code 400)
      */
     @ApiOperation(value = "Finds applications by search parameters (initially one).", nickname = "applicationStatusGetBySearchParametersPost", notes = "", response = SearchResponse.class, tags={  })
-    @ApiResponses(value = { 
+    @ApiResponses(value = {
         @ApiResponse(code = 200, message = "OK", response = SearchResponse.class),
         @ApiResponse(code = 400, message = "Invalid status value", response = SearchErrorResponse.class) })
     @RequestMapping(value = "/applicationStatus/getBySearchParameters",
-        produces = { "application/json" }, 
+        produces = { "application/json" },
         consumes = { "application/json" },
         method = RequestMethod.POST)
-    default ResponseEntity<SearchResponse> applicationStatusGetBySearchParametersPost(@ApiParam(value = "" ,required=true )  @Valid @RequestBody SearchParameters searchParameters) {
-        getRequest().ifPresent(request -> {
+    default ResponseEntity<SearchResponse> applicationStatusGetBySearchParametersPost(
+        @ApiParam(value = "" ,required=true )  @Valid @RequestHeader(HttpHeaders.AUTHORIZATION) String bearerToken,
+        @ApiParam(value = "" ,required=true )  @Valid @RequestBody SearchParameters searchParameters) {
+
+        final Optional<NativeWebRequest> nativeWebRequest = getRequest();
+        if (nativeWebRequest.isPresent()) {
+
+            final NativeWebRequest request = nativeWebRequest.get();
+
             for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
                 if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
-                    log.info("/applicationStatus/getBySearchParameters request->return mock response");
-                    String exampleString = "{\n" +
-                        "  \"messageHeader\": {\n" +
-                        "    \"consumer\": {\n" +
-                        "      \"code\": \"HMCTS\",\n" +
-                        "      \"description\": \"HM Courts and Tribunal Service\"\n" +
-                        "    },\n" +
-                        "    \"correlationId\": \"ABC2344BCED2234EA\",\n" +
-                        "    \"eventDateTime\": \"2020-07-21T17:32:28Z\"\n" +
-                        "  },\n" +
-                        "  \"messageType\": \"RESPONSE_RIGHT_OF_APPEAL_DETAILS\",\n" +
-                        "  \"status\": [\n" +
-                        "    {\n" +
-                        "      \"applicationStatus\": {\n" +
-                        "        \"applicationType\": {\n" +
-                        "          \"code\": \"ASYLUM\",\n" +
-                        "          \"description\": \"Asylum and Protection\"\n" +
-                        "        },\n" +
-                        "        \"claimReasonType\": {\n" +
-                        "          \"code\": \"HUMANRIGHTS\",\n" +
-                        "          \"description\": \"Human Rights\"\n" +
-                        "        },\n" +
-                        "        \"decisionCommunication\": {\n" +
-                        "          \"description\": \"E-mail\",\n" +
-                        "          \"dispatchDate\": \"2020-07-21T17:32:28Z\",\n" +
-                        "          \"sentDate\": \"2020-07-21T17:32:28Z\",\n" +
-                        "          \"type\": \"EMAIL\"\n" +
-                        "        },\n" +
-                        "        \"decisionDate\": \"2020-07-21T17:32:28Z\",\n" +
-                        "        \"decisionType\": {\n" +
-                        "          \"code\": \"REJECTION\",\n" +
-                        "          \"description\": \"Rejected\"\n" +
-                        "        },\n" +
-                        "        \"documentReference\": \"1234-1234-5678-5678/00\",\n" +
-                        "        \"metadata\": [\n" +
-                        "          {\n" +
-                        "            \"code\": \"APPEALABLE or DISPATCH_DATE\",\n" +
-                        "            \"valueBoolean\": true,\n" +
-                        "            \"valueDateTime\": \"2020-07-21T17:32:28Z\",\n" +
-                        "            \"valueString\": \"Some extra decision data\"\n" +
-                        "          }\n" +
-                        "        ],\n" +
-                        "        \"rejectionReasons\": [\n" +
-                        "          {\n" +
-                        "            \"reason\": \"Application not completed properly\"\n" +
-                        "          }\n" +
-                        "        ],\n" +
-                        "        \"roleSubType\": {\n" +
-                        "          \"code\": \"SPOUSE\",\n" +
-                        "          \"description\": \"Spouse\"\n" +
-                        "        },\n" +
-                        "        \"roleType\": {\n" +
-                        "          \"code\": \"DEPENDANT\",\n" +
-                        "          \"description\": \"Dependent\"\n" +
-                        "        }\n" +
-                        "      },\n" +
-                        "      \"person\": {\n" +
-                        "        \"dayOfBirth\": 29,\n" +
-                        "        \"familyName\": \"Smith\",\n" +
-                        "        \"fullName\": \"John Smith\",\n" +
-                        "        \"gender\": {\n" +
-                        "          \"code\": \"M (denoting male)\",\n" +
-                        "          \"description\": \"Male\"\n" +
-                        "        },\n" +
-                        "        \"givenName\": \"John\",\n" +
-                        "        \"monthOfBirth\": 2,\n" +
-                        "        \"nationality\": {\n" +
-                        "          \"code\": \"CAN\",\n" +
-                        "          \"description\": \"Canada\"\n" +
-                        "        },\n" +
-                        "        \"yearOfBirth\": 1968\n" +
-                        "      }\n" +
-                        "    },\n" +
-                        "    {\n" +
-                        "      \"applicationStatus\": {\n" +
-                        "        \"applicationType\": {\n" +
-                        "          \"code\": \"ASYLUM\",\n" +
-                        "          \"description\": \"Asylum and Protection\"\n" +
-                        "        },\n" +
-                        "        \"claimReasonType\": {\n" +
-                        "          \"code\": \"HUMANRIGHTS\",\n" +
-                        "          \"description\": \"Human Rights\"\n" +
-                        "        },\n" +
-                        "        \"decisionCommunication\": {\n" +
-                        "          \"description\": \"E-mail\",\n" +
-                        "          \"dispatchDate\": \"2020-07-21T17:32:28Z\",\n" +
-                        "          \"sentDate\": \"2020-07-21T17:32:28Z\",\n" +
-                        "          \"type\": \"EMAIL\"\n" +
-                        "        },\n" +
-                        "        \"decisionDate\": \"2020-07-21T17:32:28Z\",\n" +
-                        "        \"decisionType\": {\n" +
-                        "          \"code\": \"REJECTION\",\n" +
-                        "          \"description\": \"Rejected\"\n" +
-                        "        },\n" +
-                        "        \"documentReference\": \"1234-1234-5678-5678/00\",\n" +
-                        "        \"metadata\": [\n" +
-                        "          {\n" +
-                        "            \"code\": \"DISPATCH_DATE\",\n" +
-                        "            \"valueBoolean\": false,\n" +
-                        "            \"valueDateTime\": \"2020-07-21T17:32:28Z\",\n" +
-                        "            \"valueString\": \"Some extra decision data\"\n" +
-                        "          },\n" +
-                        "          {\n" +
-                        "            \"code\": \"APPEALABLE\",\n" +
-                        "            \"valueBoolean\": true,\n" +
-                        "            \"valueDateTime\": \"2020-07-21T17:32:28Z\",\n" +
-                        "            \"valueString\": \"Some extra decision data\"\n" +
-                        "          }\n" +
-                        "        ],\n" +
-                        "        \"rejectionReasons\": [\n" +
-                        "          {\n" +
-                        "            \"reason\": \"Application not completed properly\"\n" +
-                        "          },\n" +
-                        "          {\n" +
-                        "            \"reason\": \"Application not entered properly\"\n" +
-                        "          }\n" +
-                        "        ],\n" +
-                        "        \"roleSubType\": {\n" +
-                        "          \"code\": \"MAIN\",\n" +
-                        "          \"description\": \"Main\"\n" +
-                        "        },\n" +
-                        "        \"roleType\": {\n" +
-                        "          \"code\": \"APPLICANT\",\n" +
-                        "          \"description\": \"Applicant\"\n" +
-                        "        }\n" +
-                        "      },\n" +
-                        "      \"person\": {\n" +
-                        "        \"dayOfBirth\": 21,\n" +
-                        "        \"familyName\": \"Smith\",\n" +
-                        "        \"fullName\": \"Capability Smith\",\n" +
-                        "        \"gender\": {\n" +
-                        "          \"code\": \"M (denoting male)\",\n" +
-                        "          \"description\": \"Male\"\n" +
-                        "        },\n" +
-                        "        \"givenName\": \"Capability\",\n" +
-                        "        \"monthOfBirth\": 3,\n" +
-                        "        \"nationality\": {\n" +
-                        "          \"code\": \"CAN\",\n" +
-                        "          \"description\": \"Canada\"\n" +
-                        "        },\n" +
-                        "        \"yearOfBirth\": 1970\n" +
-                        "      }\n" +
-                        "    }\n" +
-                        "  ]\n" +
-                        "}";
-                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
+
+                    log.info("/applicationStatus/getBySearchParameters nativeWebRequest->return mock response");
+
+                    final String documentReference = searchParameters.getSearchParams().get(0).getSpValue();
+
+                    if (documentReference.equalsIgnoreCase("1212/009900362015")
+                        || documentReference.equalsIgnoreCase("2/032323")) {
+                        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                    }
+
+                    String responseJson = null;
+                    try {
+                        responseJson = getResponseJson(documentReference);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    String exampleString = "";
+                    ApiUtil.setExampleResponse(request, "application/json", responseJson);
+                    break;
+                }
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * POST /token : returns JWT access_token for given client_id and client_secret..
+     *
+     * @param searchParameters  (required)
+     * @return OK (status code 200)
+     *         or Invalid status value (status code 400)
+     */
+    @ApiOperation(value = "get acces token.", nickname = "tokenPost", notes = "", response = String.class, tags={  })
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK", response = String.class)
+    })
+    @RequestMapping(value = "/token",
+        produces = { "application/json" },
+        consumes = { "application/x-www-form-urlencoded" },
+        method = RequestMethod.POST)
+    default ResponseEntity<SearchResponse> tokenPost(
+        @ApiParam(value = "", required=true )  @Valid @RequestBody MultiValueMap<String, String> requestBody) {
+
+        getRequest().ifPresent(request -> {
+            for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/x-www-form-urlencoded"))) {
+                    log.info("/token request->return mock response");
+
+                    String jwtToken = "{"
+                        + "\"access_token\": \"some_access_token\","
+                        + "\"expires_in\": 300,"
+                        + "\"token_type\": \"bearer\","
+                        + "\"not-before-policy\": 0,"
+                        + "\"scope\": \"email profile\""
+                        + "}";
+                    ApiUtil.setExampleResponse(request, "application/json", jwtToken);
                     break;
                 }
             }
         });
         return new ResponseEntity<>(HttpStatus.OK);
 
+    }
+
+    @NotNull
+    private String getResponseJson(String homeOfficeReference) throws IOException {
+
+        String responseJsonFile;
+
+        switch (homeOfficeReference) {
+            case "1212-0099-0061-7075": // Valid UAN reference
+                responseJsonFile = "ho-api_get-by-search-params_200-valid_response-01.json";
+                break;
+            case "1212-0099-0062-8083":
+                responseJsonFile = "ho-api_get-by-serach-params_200_valid-response-02.json";
+                break;
+            case "020639760": // CID reference response
+                responseJsonFile = "ho-api_get-by-search-params_200-valid_response-01.json";
+                break;
+            case "020651434": // CID reference - not Asylum case response
+                responseJsonFile = "ho-api_get-by-search-params_200-valid_response-01.json";
+                break;
+            case "1212-0099-0029-3029": // Valid UAN - no data found
+                responseJsonFile = "ho_api_get-by-search-params_200_no-data-found-uan-ref.json";
+                break;
+            case "123123123": // Valid CID - no data found
+                responseJsonFile = "ho_api_get-by-search-params_200_no-data-found-uan-ref.json";
+                break;
+            default:
+                throw new IOException("Invalid home office reference : " + homeOfficeReference);
+        }
+
+        final File hoSearchResponseResourceFile
+            = ResourceUtils.getFile("classpath:" + responseJsonFile);
+
+        return new String(Files.readAllBytes(hoSearchResponseResourceFile.toPath()));
     }
 
 }
