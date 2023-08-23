e6fd3c5d22a387ceb4559e1f43455e6fcd69bdcd
BurnsT <tim.burns@HMCTS.net>
2023-08-23 17:00:01 +0100

    RIA-6817 - dealing with swagger deprecation issue

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/iahomeofficemockapi/infrastructure/testutils/SpringBootIntegrationTest.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.reform.iahomeofficemockapi.infrastructure.testutils;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.common.Slf4jNotifier;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.reform.iahomeofficemockapi.generated.infrastructure.api.invoker.OpenAPI2SpringBoot;
+
+@SpringBootTest(classes = {
+    OpenAPI2SpringBoot.class
+})
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("integration")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SpringBootIntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    protected static WireMockServer server;
+
+    @BeforeAll
+    public void spinUp() {
+        server = new WireMockServer(WireMockConfiguration.options()
+                                        .notifier(new Slf4jNotifier(true))
+                                        .port(8990));
+        server.start();
+    }
+
+    @AfterEach
+    public void reset() {
+        server.resetAll();
+    }
+
+    @AfterAll
+    public void shutDown() {
+        server.stop();
+    }
+
+}
